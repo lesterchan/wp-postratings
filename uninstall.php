0631d777f9f0b6a50d7fb3e35163430f2952eb3e
Lester Chan <lesterchan@gmail.com>
2016-12-15 13:27:24 +0800

    Fix deprcated wp_get_sites

--- uninstall.php
+++ uninstall.php
@@ -29,23 +29,24 @@ $option_names = array(
 
 
 if ( is_multisite() ) {
-	$ms_sites = wp_get_sites();
+	$ms_sites = function_exists( 'get_sites' ) ? get_sites() : wp_get_sites();
 
-	if( 0 < sizeof( $ms_sites ) ) {
+	if ( 0 < count( $ms_sites ) ) {
 		foreach ( $ms_sites as $ms_site ) {
-			switch_to_blog( $ms_site['blog_id'] );
-			if( sizeof( $option_names ) > 0 ) {
-				foreach( $option_names as $option_name ) {
+			$blog_id = isset( $ms_site['blog_id'] ) ? $ms_site['blog_id'] : $ms_site->blog_id;
+			switch_to_blog( $blog_id );
+			if ( count( $option_names ) > 0 ) {
+				foreach ( $option_names as $option_name ) {
 					delete_option( $option_name );
 					plugin_uninstalled();
+					restore_current_blog();
 				}
 			}
-			restore_current_blog();
 		}
 	}
 } else {
-	if( sizeof( $option_names ) > 0 ) {
-		foreach( $option_names as $option_name ) {
+	if ( count( $option_names ) > 0 ) {
+		foreach ( $option_names as $option_name ) {
 			delete_option( $option_name );
 			plugin_uninstalled();
 		}
