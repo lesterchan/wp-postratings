240a9b4a6ee8f8f29115d836c8c2b3dd5ca297d8
Lester Chan <lesterchan@gmail.com>
2017-04-21 16:34:52 +0800

    Check AMP endpoint

--- includes/postratings-shortcodes.php
+++ includes/postratings-shortcodes.php
@@ -18,16 +18,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ### Function: Short Code For Inserting Ratings Into Posts
 function ratings_shortcode( $atts ) {
-    $attributes = shortcode_atts( array( 'id' => 0, 'results' => false ), $atts );
-    if( ! is_feed() ) {
-        $id = intval( $attributes['id'] );
-        if( $attributes['results'] ) {
-            return the_ratings_results( $id );
-        } else {
-            return the_ratings( 'span', $id, false );
-        }
-    } else {
-        return esc_html__( 'Note: There is a rating embedded within this post, please visit this post to rate it.', 'wp-postratings' );
-    }
+	$attributes = shortcode_atts( array( 'id' => 0, 'results' => false ), $atts );
+	if( ! is_feed() && ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) ) {
+		$id = (int)$attributes['id'];
+		if( $attributes['results'] ) {
+			return the_ratings_results( $id );
+		}
+
+		return the_ratings( 'span', $id, false );
+	}
+
+	return esc_html__( 'Note: There is a rating embedded within this post, please visit this post to rate it.', 'wp-postratings' );
 }
 add_shortcode( 'ratings', 'ratings_shortcode' );
