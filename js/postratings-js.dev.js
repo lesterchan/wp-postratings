0afe165a8ab4ff01d6dba0b1e09f6bf3a6684f78
Raphaël Droz <raphael.droz+floss@gmail.com>
2017-07-05 20:03:11 -0300

    code cleanup
    * avoid code/routine duplication
      especially for postratings_page_admin_most_stats() and get_ratings_images_vote()
    * use ternary operator where it improve readability
    * use here-doc where possible
    * move wp_localize_script() where the_rating() actually display voting form
      and move part of its logic client-side => code simplification
    * use document.getElementById() where it makes things simpler
      and alias jQuery to $j
    * move some "static" file_exists() outside of loops and use a cleaner
      way to declare/implode() complex markup
    * avoid double-escaping, let this task to wpdb() + use wpdb->insert()

--- js/postratings-js.dev.js
+++ js/postratings-js.dev.js
@@ -17,65 +17,59 @@
 
 
 // Variables
-var post_id = 0;
-var post_rating = 0;
+var $j = jQuery.noConflict();
 var is_being_rated = false;
 ratingsL10n.custom = parseInt(ratingsL10n.custom);
 ratingsL10n.max = parseInt(ratingsL10n.max);
 ratingsL10n.show_loading = parseInt(ratingsL10n.show_loading);
 ratingsL10n.show_fading = parseInt(ratingsL10n.show_fading);
 
+
+var ratings_mouseover_image;
+if(ratingsL10n.custom) {
+	ratings_mouseover_image = [];
+        for(var i = 1; i <= ratingsL10n.max; i++) {
+		ratings_mouseover_image[i] = new Image();
+		ratings_mouseover_image[i].src = ratingsL10n.plugin_url + "/images/" + ratingsL10n.image + "/rating_" + i + "_over." + ratingsL10n.image_ext ;
+        }
+} else {
+	ratings_mouseover_image = new Image();
+	ratings_mouseover_image.src = ratingsL10n.plugin_url + "/images/" + ratingsL10n.image + "/rating_over."  + ratingsL10n.image_ext ;
+}
+
 // When User Mouse Over Ratings
-function current_rating(id, rating, rating_text) {
+function current_rating(post_id, post_rating, rating_text) {
 	if(!is_being_rated) {
-		post_id = id;
-		post_rating = rating;
 		if(ratingsL10n.custom && ratingsL10n.max == 2) {
-			jQuery('#rating_' + post_id + '_' + rating).attr('src', eval('ratings_' + rating + '_mouseover_image.src'));
+			document.getElementById('rating_' + post_id + '_' + post_rating).src = ratings_mouseover_image[i].src;
 		} else {
-			for(i = 1; i <= rating; i++) {
-				if(ratingsL10n.custom) {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', eval('ratings_' + i + '_mouseover_image.src'));
-				} else {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', ratings_mouseover_image.src);
-				}
+			for(var i = 1; i <= post_rating; i++) {
+				console.log("#" + 'rating_' + post_id + '_' + i + ' = ' + ratings_mouseover_image.src);
+				document.getElementById('rating_' + post_id + '_' + i).src = ratingsL10n.custom ? ratings_mouseover_image[i].src : ratings_mouseover_image.src;
 			}
 		}
 		if(jQuery('#ratings_' + post_id + '_text').length) {
-			jQuery('#ratings_' + post_id + '_text').show();
-			jQuery('#ratings_' + post_id + '_text').html(rating_text);
+			jQuery('#ratings_' + post_id + '_text').html(rating_text).show();
 		}
 	}
 }
 
-
 // When User Mouse Out Ratings
-function ratings_off(rating_score, insert_half, half_rtl) {
+function ratings_off(post_id, rating_score, insert_half, half_rtl) {
 	if(!is_being_rated) {
-		for(i = 1; i <= ratingsL10n.max; i++) {
+		var baseimg = ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating' ;
+		for(var i = 1; i <= ratingsL10n.max; i++) {
+			var element = document.getElementById('rating_' + post_id + '_' + i);
 			if(i <= rating_score) {
-				if(ratingsL10n.custom) {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating_' + i + '_on.' + ratingsL10n.image_ext);
-				} else {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating_on.' + ratingsL10n.image_ext);
-				}
+				element.src = baseimg + (ratingsL10n.custom ? '_' + i : '') + '_on.' + ratingsL10n.image_ext;
 			} else if(i == insert_half) {
-				if(ratingsL10n.custom) {
-					jQuery('#rating_' + post_id + '_' + i).attr('src',  ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating_' + i + '_half' + (half_rtl ? '-rtl' : '') + '.' + ratingsL10n.image_ext);
-				} else {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating_half' + (half_rtl ? '-rtl' : '') + '.' + ratingsL10n.image_ext);
-				}
+				element.src = baseimg + (ratingsL10n.custom ? '_' + i : '') + '_half' + (half_rtl ? '-rtl' : '') + '.' + ratingsL10n.image_ext;
 			} else {
-				if(ratingsL10n.custom) {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating_' + i + '_off.' + ratingsL10n.image_ext);
-				} else {
-					jQuery('#rating_' + post_id + '_' + i).attr('src', ratingsL10n.plugin_url + '/images/' + ratingsL10n.image + '/rating_off.' + ratingsL10n.image_ext);
-				}
+				element.src = baseimg + (ratingsL10n.custom ? '_' + i : '') + '_off.' + ratingsL10n.image_ext;
 			}
 		}
 		if(jQuery('#ratings_' + post_id + '_text').length) {
-			jQuery('#ratings_' + post_id + '_text').hide();
-			jQuery('#ratings_' + post_id + '_text').empty();
+			jQuery('#ratings_' + post_id + '_text').hide().empty();
 		}
 	}
 }
@@ -86,13 +80,13 @@ function set_is_being_rated(rated_status) {
 }
 
 // Process Post Ratings Success
-function rate_post_success(data) {
-	jQuery('#post-ratings-' + post_id).html(data);
+function rate_post_success(post_id, data) {
+	$j('#post-ratings-' + post_id).html(data);
 	if(ratingsL10n.show_loading) {
-		jQuery('#post-ratings-' + post_id + '-loading').hide();
+		$j('#post-ratings-' + post_id + '-loading').hide();
 	}
 	if(ratingsL10n.show_fading) {
-		jQuery('#post-ratings-' + post_id).fadeTo('def', 1, function () {
+		$j('#post-ratings-' + post_id).fadeTo('def', 1, function () {
 			set_is_being_rated(false);
 		});
 	} else {
@@ -101,27 +95,33 @@ function rate_post_success(data) {
 }
 
 // Process Post Ratings
-function rate_post() {
-	post_ratings_el = jQuery('#post-ratings-' + post_id);
+function rate_post(post_id, post_rating) {
+	var post_ratings_el = $j('#post-ratings-' + post_id);
 	if(!is_being_rated) {
-		post_ratings_nonce = jQuery(post_ratings_el).data('nonce');
+		var post_ratings_nonce = $j(post_ratings_el).data('nonce');
 		if(typeof post_ratings_nonce == 'undefined' || post_ratings_nonce == null)
-			post_ratings_nonce = jQuery(post_ratings_el).attr('data-nonce');
+			post_ratings_nonce = $j(post_ratings_el).attr('data-nonce');
 		set_is_being_rated(true);
 		if(ratingsL10n.show_fading) {
-			jQuery(post_ratings_el).fadeTo('def', 0, function () {
+			$j(post_ratings_el).fadeTo('def', 0, function () {
 				if(ratingsL10n.show_loading) {
-					jQuery('#post-ratings-' + post_id + '-loading').show();
+					$j('#post-ratings-' + post_id + '-loading').show();
 				}
-				jQuery.ajax({type: 'POST', xhrFields: {withCredentials: true}, dataType: 'html', url: ratingsL10n.ajax_url, data: 'action=postratings&pid=' + post_id + '&rate=' + post_rating + '&postratings_' + post_id + '_nonce=' + post_ratings_nonce, cache: false, success: rate_post_success});
 			});
 		} else {
 			if(ratingsL10n.show_loading) {
-				jQuery('#post-ratings-' + post_id + '-loading').show();
+				$j('#post-ratings-' + post_id + '-loading').show();
 			}
-			jQuery.ajax({type: 'POST', xhrFields: {withCredentials: true}, dataType: 'html', url: ratingsL10n.ajax_url, data: 'action=postratings&pid=' + post_id + '&rate=' + post_rating + '&postratings_' + post_id + '_nonce=' + post_ratings_nonce, cache: false, success: rate_post_success});
 		}
-	} else {
+		$j.post({xhrFields: {withCredentials: true},
+			 dataType: 'html',
+			 url: ratingsL10n.ajax_url,
+			 data: 'action=postratings&pid=' + post_id + '&rate=' + post_rating + '&postratings_' + post_id + '_nonce=' + post_ratings_nonce,
+			 cache: false})
+			.done(function(data) { rate_post_success(post_id, data); });
+	}
+
+	else {
 		alert(ratingsL10n.text_wait);
 	}
 }
