7ae52c117e62e138c0857de717cdc69d6c235886
Michael Krieger <phyre@phyre.me>
2019-01-11 15:01:53 -0500

    Update wp-postratings.php

--- wp-postratings.php
+++ wp-postratings.php
@@ -1163,10 +1163,10 @@ function expand_ratings_template($template, $post_data, $post_ratings_data = nul
 
 	// Post Template Variables
 	$post_link = get_permalink( $post_data );
-	if ((get_post_meta($post->ID, ‘_yoast_wpseo_title’, true)) == “”) {
+	if ((get_post_meta($post_id, '_yoast_wpseo_title', true)) == “”) {
 		$post_title = get_the_title($post_data);
 	} else {
-		$post_title = get_post_meta($post_id, ‘_yoast_wpseo_title’, true);
+		$post_title = get_post_meta($post_id, '_yoast_wpseo_title', true);
 	}
 	if ( $max_post_title_chars > 0 ) {
 		$post_title = snippet_text( $post_title, $max_post_title_chars );
@@ -1201,8 +1201,8 @@ function expand_ratings_template($template, $post_data, $post_ratings_data = nul
 		$value = str_replace( '%POST_THUMBNAIL%', get_the_post_thumbnail( $post, 'thumbnail' ), $value );
 	}
 	
-	if (get_post_meta(get_the_ID(), ‘_yoast_wpseo_metadesc’, true) <> “”) {
-		$post_excerpt = get_post_meta(get_the_ID(), ‘_yoast_wpseo_metadesc’, true);
+	if (get_post_meta($post_id, '_yoast_wpseo_metadesc', true) <> “”) {
+		$post_excerpt = get_post_meta($post_id, '_yoast_wpseo_metadesc', true);
 	}
 
 	// Google Rich Snippet
